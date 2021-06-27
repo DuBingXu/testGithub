992e1d342da0d15139bf02d0d077f7d3b440c817
du-github <adubingxu@163.com>
2021-06-27 16:34:50 +0800

    第三次提交

--- yuan/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include "hello.h"
-
-void printHello(){
-    cout << "hello world!!!" << endl;
-    cout << "hello world!!!" << endl;
-    cout << "hello world!!!" << endl;
-    cout << "hello world!!!" << endl;
-    cout << "hello world!!!" << endl;
-}
